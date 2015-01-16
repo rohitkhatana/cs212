e932e510022fea1a5977dc9b657dfe460ec10a92
Rohit Khatana <rohitkhatana.khatana@gmail.com>
2015-01-17 03:24:19 +0530

    document the poker details

--- poker.py
+++ poker.py
@@ -1,4 +1,24 @@
+'''
+auther - Rohit Khatana
+card Number
+1-9, T =10
+Suits
+C = Clubs
+D = Diamonds
+H = Hearts
+S = Spades
 
+The Rule of poker
+0 - Nothing 
+1 - Pair(1 pair)
+2 - Two Pair(pair of two card)
+3 - Three of a Kind(3 cards of same rank(no))
+4 - Straight(5 card in sequtional order, suit does not matter)
+5 - Flush(5 cards of same suit, rank dont matter)
+6 - Full House(3 card of same number)(2C, 2H, 2D,3D, 2D)
+7 - Four of a Kind(2C,5C,JC,7C,7H)
+8 - Straight Flush(9H,TH,JH,QH,KH)
+'''
 #max(iteratble_object, key=func)==>good to know
 
 def poker(hands)
@@ -6,3 +26,8 @@ def poker(hands)
 
 def hand_rank(hand):
     pass
+
+def test():
+    "test case for the functions in poker program"
+    sf = "6C 7C 8C 9C TC".split()
+
